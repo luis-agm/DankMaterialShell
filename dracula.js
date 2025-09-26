e6c6a36821c8c8290edef6a3f605fcb1e674c797
Luis González <5774777+luis-agm@users.noreply.github.com>
2025-09-27 00:26:01 +0200

    --wip-- [skip ci]

---
+++ dracula.js
@@ -0,0 +1,63 @@
+const DraculaClassic = {
+    surface: "#44475A",
+    surfaceText: "#F8F8F2",
+    surfaceVariant: "#343746",
+    surfaceVariantText: "#6272A4",
+    background: "#282A36",
+    backgroundText: "#F8F8F2",
+    outline: "#6272A4",
+    surfaceContainer: "#44475A",
+    surfaceContainerHigh: "#343746",
+    surfaceContainerHighest: "#21222C"
+}
+
+const DraculaAlucard = {
+    surface: "#DEDCCF",
+    surfaceText: "#1F1F1F",
+    surfaceVariant: "#EFEDDC",
+    surfaceVariantText: "#6C664B",
+    background: "#FFFBEB",
+    backgroundText: "#1F1F1F",
+    outline: "#6C664B",
+    surfaceContainer: "#DEDCCF",
+    surfaceContainerHigh: "#CECCC0",
+    surfaceContainerHighest: "#B8B6AC"
+}
+
+const DraculaVariants = {
+    "dracula-red": {
+        name: "Red",
+        dark: { primary: "#FF5555", secondary: "#FF6E6E", primaryText: "#282A36", primaryContainer: "#6F2438", surfaceTint: "#FF5555" },
+        light: { primary: "#CB3A2A", secondary: "#D74C3D", primaryText: "#FFFBEB", primaryContainer: "#F6D0D6", surfaceTint: "#CB3A2A" }
+    },
+    "dracula-orange": {
+        name: "Orange",
+        dark: { primary: "#FFB86C", secondary: "#FFFFA5", primaryText: "#282A36", primaryContainer: "#734226", surfaceTint: "#FFB86C" },
+        light: { primary: "#A34D14", secondary: "#9E841A", primaryText: "#FFFBEB", primaryContainer: "#FFE4D5", surfaceTint: "#A34D14" }
+    },
+    "dracula-yellow": {
+        name: "Yellow",
+        dark: { primary: "#F1FA8C", secondary: "#FFFFA5", primaryText: "#282A36", primaryContainer: "#6E5A2F", surfaceTint: "#F1FA8C" },
+        light: { primary: "#846E15", secondary: "#9E841A", primaryText: "#FFFBEB", primaryContainer: "#FFF6D6", surfaceTint: "#846E15" }
+    },
+    "dracula-green": {
+        name: "Green",
+        dark: { primary: "#50FA7B", secondary: "#69FF94", primaryText: "#282A36", primaryContainer: "#2F5F36", surfaceTint: "#50FA7B" },
+        light: { primary: "#14710A", secondary: "#198D0C", primaryText: "#FFFBEB", primaryContainer: "#DFF4E0", surfaceTint: "#14710A" }
+    },
+    "dracula-cyan": {
+        name: "Cyan",
+        dark: { primary: "#8BE9FD", secondary: "#A4FFFF", primaryText: "#282A36", primaryContainer: "#24586A", surfaceTint: "#8BE9FD" },
+        light: { primary: "#036A96", secondary: "#047FB4", primaryText: "#FFFBEB", primaryContainer: "#DBF1FB", surfaceTint: "#036A96" }
+    },
+    "dracula-purple": {
+        name: "Purple",
+        dark: { primary: "#BD93F9", secondary: "#D6ACFF", primaryText: "#282A36", primaryContainer: "#3F4481", surfaceTint: "#BD93F9" },
+        light: { primary: "#644AC9", secondary: "#7862D0", primaryText: "#FFFBEB", primaryContainer: "#E5E8FF", surfaceTint: "#644AC9" }
+    },
+    "dracula-pink": {
+        name: "Pink",
+        dark: { primary: "#FF79C6", secondary: "#FF92DF", primaryText: "#282A36", primaryContainer: "#7A3F69", surfaceTint: "#FF79C6" },
+        light: { primary: "#A3144D", secondary: "#BF185A", primaryText: "#FFFBEB", primaryContainer: "#F7D7EE", surfaceTint: "#A3144D" }
+    }
+}
